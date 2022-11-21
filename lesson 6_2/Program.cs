4f6faf72b65ca62f68228997d8c47ea9db3d4400
Leonid <rockwil91@gmail.com>
2022-11-21 22:33:49 +0200

    Задачаб проверка на треугольник

---
+++ lesson 6_2/Program.cs
@@ -0,0 +1,17 @@
+﻿int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int a = ReadInt("Введите размер стороны a");
+int b = ReadInt("Введите размер стороны b");
+int c = ReadInt("Введите размер стороны c");
+
+bool CheckSize (int checking, int first, int second)
+{
+    return checking < first + second;
+}
+
+if (CheckSize (a, b, c) && CheckSize (b, a,c) && CheckSize (c, a, b)) Console.WriteLine("Треугольнику есть место быть");
+else Console.WriteLine("Такого треугольника не существует");
