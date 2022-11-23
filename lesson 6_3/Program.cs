e02a84b19752078b1c89dae8eaf025d4ef041738
Leonid <rockwil91@gmail.com>
2022-11-23 22:39:11 +0200

    Д/З 6 - Задача 41

---
+++ lesson 6_3/Program.cs
@@ -0,0 +1,22 @@
+﻿int ReadInt (string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = ReadInt("Введите число: ");
+int count = 0;
+int[] binar = new int[ArraySize(num)];
+
+
+int ArraySize (int number)
+{
+    
+    for (int i = num; i > 1; i /= 2)
+    {
+        count++;
+    }
+    return count -= 1;
+}
+
+Console.WriteLine($"{ArraySize(num)}");
---
+++ task 0/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 4*/
+
+
+string m = ReadInt("Введите числа через запятую: ") + ',';
+int[] numArray = GetArray(m, ',');
+int evenNum = 0;
+
+string ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    return Console.ReadLine();
+}
+
+
+int[] GetArray (string input, char name)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == name)
+            count++;
+    }
+    int[] array = new int[count];
+    int index = 0;
+    string temp = string.Empty;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] != name)
+            temp += input[i];
+        else
+        {
+            array[index] = Convert.ToInt32(temp);
+            temp = string.Empty;
+            index++;
+        }    
+    }
+    return array;
+}
+
+for (int i = 0; i < numArray.Length; i++)
+{
+    if (numArray[i] > 0)
+        evenNum++;
+}
+
+void Print(string result)
+{
+    Console.WriteLine($"{result} -> {evenNum}");
+}
+
+Print(m);
